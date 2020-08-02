7b11a5004cd2c22aa2fb03b994d164783e0c7aff
Naren Karn <narenkarn249@gmail.com>
2020-08-02 17:33:16 +0545

    First Commit

---
+++ Dainikprashashan/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿
+using Dainikprashashan.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dainikprashashan.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet] //Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost] //Register
+        public ActionResult Register(Login l)
+        {
+            var lservice = new Loginservice();
+            var response = lservice.SaveRegister(l);
+            return RedirectToAction("Register", "Login");
+        }
+
+        [HttpGet] //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost] //Login
+        public ActionResult Login(Login lo)
+        {
+            var loginsuccess = new Loginservice();
+            var response = loginsuccess.SaveLogin(lo);
+            var LOGINiD = response.Item2;
+            Session["LoginId"] = response.Item2;
+            if (response.Item1 == "true")
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+    }
+}
---
+++ Dainikprashashan/Service/Loginservice.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dainikprashashan.Service
+{
+    public class Loginservice
+    {
+        //Register
+        public string SaveRegister(Login ml)
+        {
+            var message = "";
+            using (var Context = new Entities1())
+            {
+                var user = new Login()
+                {
+                    Username = ml.Username,
+                    Password = ml.Password,
+                    Confirm_Password = ml.Confirm_Password
+                };
+                Context.Logins.Add(user);
+
+                Context.SaveChanges();
+                message = "save successfull";
+            }
+            return message;
+        }
+
+        //Login
+        public Tuple<string, int> SaveLogin(Login ll)
+        {
+            var message = "";
+            using (var Context=new Entities1())
+            {
+                var loginData = Context.Logins
+                          .Where(lg => lg.Username == ll.Username && lg.Password == ll.Password)
+                          .FirstOrDefault<Login>();
+                if (loginData.Username != "")
+                {
+                    message = "true";
+                }
+                else
+                {
+                    message = "false";
+                }
+                return Tuple.Create(message, Convert.ToInt32(loginData.UserId));
+            }
+        }
+    }
+}
